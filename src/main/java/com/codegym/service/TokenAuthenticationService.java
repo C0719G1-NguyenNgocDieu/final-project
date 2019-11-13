d9a1481b38d7c6cf8d061e33cd0f6cc5ed46ad82
Nhat Nguyen <minhnhat2807@gmail.com>
2019-11-13 22:00:48 +0700

    thay doi cross domain, token trả về trong body dạng json format

--- src/main/java/com/codegym/service/TokenAuthenticationService.java
+++ src/main/java/com/codegym/service/TokenAuthenticationService.java
@@ -1,17 +1,22 @@
 package com.codegym.service;
 
 
-import java.util.Collections;
-import java.util.Date;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TokenAuthenticationService {
 
@@ -23,11 +28,28 @@ public class TokenAuthenticationService {
 
     static final String HEADER_STRING = "Authorization";
 
-    public static void addAuthentication(HttpServletResponse res, String username) {
+    public static void addAuthentication(HttpServletResponse res, String username) throws IOException {
         String JWT = Jwts.builder().setSubject(username)
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
                 .signWith(SignatureAlgorithm.HS512, SECRET).compact();
-        res.addHeader(HEADER_STRING, TOKEN_PREFIX + " " + JWT);
+
+        // Nên trả token dạng json thay vì trong header
+        // Có thể trả thêm nhiều data như refrestToken, thông tin user login như name, email, bithday, ... (không cần gọi thêm request)
+        //res.addHeader(HEADER_STRING, TOKEN_PREFIX + " " + JWT);
+
+        Map<String, String> tokenMap = new HashMap<String, String>();
+        tokenMap.put("token", JWT);
+
+        res.setStatus(HttpStatus.OK.value());
+        res.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        //create Json Object
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        PrintWriter out = res.getWriter();
+
+        out.print(objectMapper.writeValueAsString(tokenMap));
+        out.flush();
     }
 
     public static Authentication getAuthentication(HttpServletRequest request) {
